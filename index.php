a627c1b172e736f7ba563c68827c240d6bbd4026
André Sousa <andre.sousa40@etec.sp.gov.br>
2019-03-24 18:35:20 -0300

    Métodos de listagem de usuários

--- index.php
+++ index.php
@@ -2,10 +2,29 @@
 
     require_once("config.php");
 
-    $root = new Usuario();
-    
-    $root->loadById(3);
+    #Carrega um usuário por vez
+    /*
+     * $root = new Usuario();    
+     * $root->loadById(3);
+     */
 
-    echo $root;
+    #Carrega uma lista de usuários
+    /*
+     * $lista = Usuario::getList();
+     * echo json_encode($lista); 
+     */
+
+    #Carrega uma lista de usuários buscando pelo login
+     /*
+      * $search = Usuario::search("jo");
+      * echo json_encode($search);
+      */
+    
+    #Carrega um usuário usando o login e a senha
+    $usuario = new Usuario();
+    $usuario->login("root", "!@#$");
+
+    echo $usuario;
+     
 
 ?>
